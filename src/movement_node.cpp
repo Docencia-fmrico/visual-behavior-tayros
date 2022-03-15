ab1b88da083de796a7963bc327967680cc43818c
Saúl Navajas Quijano <s.navajas.2020@alumnos.urjc.es>
2022-03-15 18:25:35 +0100

    avances en la rama movement_branch
    Signed-off-by: Saúl Navajas Quijano <s.navajas.2020@alumnos.urjc.es>

---
+++ src/movement_node.cpp
@@ -0,0 +1,34 @@
+// Copyright 2021 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "visual_behaviour/Movement.h"
+#include "ros/ros.h"
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "movement");
+
+  visual_behaviour::Movement visual_behaviour;
+
+  ros::Rate loop_rate(1);
+  while (ros::ok())
+  {
+    visual_behaviour.MoveRobot();
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
